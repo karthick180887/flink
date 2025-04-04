d6d2763e89e514ad098ada99525fafbfdc9ce448
karthick180887 <karthickspikey@gmail.com>
2025-04-04 11:44:25 +0530

    Update example1.java

--- example1.java
+++ example1.java
@@ -4,7 +4,7 @@ import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-public class example1.java {
+public class example1 {
 
     public static void main(String[] args) throws Exception {
 
