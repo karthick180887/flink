3d6d7b4929c94bcef1c4e2e58d75adf08efdad4a
karthick180887 <karthickspikey@gmail.com>
2025-04-04 10:27:59 +0530

    Add files via upload

---
+++ HighSpeedDetection_v1.java
@@ -0,0 +1,41 @@
+package com.pluralsight.streaming;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class HighSpeedDetection {
+
+    public static void main(String[] args) throws Exception {
+
+        final StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<String> dataStream = env
+                .socketTextStream("localhost", 9000)
+                .filter(new Filter());
+
+        dataStream.print();
+
+        env.execute("Car speed detection");
+    }
+
+    public static class Filter implements FilterFunction<String> {
+
+        public boolean filter(String input) throws Exception {
+            try {
+                String[] tokens = input.split(",");
+
+                if (tokens.length == 2 && Float.parseFloat(tokens[1].trim()) > 65) {
+                    return true;
+                }
+            } catch (Exception ex) {
+            }
+
+            return false;
+        }
+
+
+    }
+
+}
