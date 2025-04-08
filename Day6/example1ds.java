9f514bafd11a98a48e995b9b6afa8a033a35922c
karthick180887 <karthickspikey@gmail.com>
2025-04-08 09:30:41 +0530

    Add files via upload

---
+++ Day6/example1ds.java
@@ -0,0 +1,40 @@
+package org.apache.flink;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.ServerSocket;
+public class example1ds
+{
+	public static void main(String[] args) throws IOException
+	{
+		ServerSocket listener = new ServerSocket(9090);
+		try{
+				Socket socket = listener.accept();
+				System.out.println("Got new connection: " + socket.toString());
+				
+				BufferedReader br = new BufferedReader(new FileReader("/home/flink/flink-day3/src/main/resources/example1.txt"));
+				
+				try {
+					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+					String line;
+					while ((line = br.readLine()) != null){
+						
+						out.println(line);
+						Thread.sleep(50);
+					}
+					
+				} finally{
+					socket.close();
+				}
+			
+		} catch(Exception e ){
+			e.printStackTrace();
+		} finally{
+			
+			listener.close();
+		}
+	}
+}
+
