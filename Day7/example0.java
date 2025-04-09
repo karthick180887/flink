8810ad4533a92ee8d5e67e0995e00c74bed01abf
karthick180887 <karthickspikey@gmail.com>
2025-04-09 10:33:57 +0530

    Add files via upload

---
+++ Day7/example0.java
@@ -0,0 +1,22 @@
+package org.apache.flink;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+import java.util.Arrays;
+public class example0 {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(2);
+        env.socketTextStream("localhost", 4567)
+                .flatMap((String line, Collector<Tuple2<String, Integer>> collector) -> {
+                    String[] words = line.split("\\s+");
+                    Arrays.stream(words).map(w -> Tuple2.of(w, 1))
+                            .forEach(collector::collect);
+                }).returns(Types.TUPLE(Types.STRING, Types.INT))
+                .keyBy(t -> t.f0)
+                .sum(1)
+                .print();
+        env.execute("word count from socket");
+    }
+}
