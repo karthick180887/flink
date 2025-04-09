cbc3a09787d3f3f9b84677c5b98ffff9bee54785
karthick180887 <karthickspikey@gmail.com>
2025-04-09 12:39:54 +0530

    Add files via upload

---
+++ Day7/CityTemperature.java
@@ -0,0 +1,66 @@
+package org.apache.flink;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CityTemperature {
+    @JsonProperty("CITY")
+    private String city;
+    @JsonProperty("TEMP")
+    private float temperature;
+    @JsonProperty("TIME")
+    private long timestamp;
+
+    public CityTemperature() {}
+
+    public CityTemperature(String city, float temperature, long timestamp) {
+        this.city = city;
+        this.temperature = temperature;
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "CityTemperature(city=%s, temperature=%.1f, timestamp=%d)",
+                city, temperature, timestamp
+        );
+    }
+
+    // Manual builder implementation
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String city;
+        private float temperature;
+        private long timestamp;
+
+        public Builder city(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Builder temperature(float temperature) {
+            this.temperature = temperature;
+            return this;
+        }
+
+        public Builder timestamp(long timestamp) {
+            this.timestamp = timestamp;
+            return this;
+        }
+
+        public CityTemperature build() {
+            return new CityTemperature(city, temperature, timestamp);
+        }
+    }
+
+    // Getters and setters
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+    public float getTemperature() { return temperature; }
+    public void setTemperature(float temperature) { this.temperature = temperature; }
+    public long getTimestamp() { return timestamp; }
+    public void setTimestamp(long timestamp) { this.timestamp = timestamp; }
+}
